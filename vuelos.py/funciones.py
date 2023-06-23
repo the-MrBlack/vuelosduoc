26159057c11599b2270a9d0dd9cb120e81e83f91
python2023 <k.alexanderignacio@gmail.com>
2023-06-23 16:11:49 -0400

    se agregaron nuevas def

--- vuelos.py/funciones.py
+++ vuelos.py/funciones.py
@@ -8,7 +8,7 @@ def crearmatrix(filas,columnas):
             PuestosdeAvion[columna].append([(CONTADOR)])
             CONTADOR+=1
     return PuestosdeAvion
-puestosdeAvion=crearmatrix(7,6)
+#puestosdeAvion=crearmatrix(7,6)
 
 #------------------------------------------------------------------------------------
 def DatosPasajero():
@@ -18,7 +18,7 @@ def DatosPasajero():
     bancopasajero=(input("Ingrese su  Banco:")).upper()
     basededatospasajero=[Nombre,rutPasajero,telefono,bancopasajero]
     return basededatospasajero
-DatosUsuario=DatosPasajero()
+#DatosUsuario=DatosPasajero()
 
 
     
@@ -28,52 +28,34 @@ def preciosVuelos():
     precioVIP=240000
     BaseDatosPrecios=[precioNormal,precioVIP]
     return BaseDatosPrecios
-precios=preciosVuelos()
+#precios=preciosVuelos()
 
-print(precios)
+#print(precios)
 
 #------------------------------------------------------------------------------------
-def Menu():
-    
-    opc1=int(input("1.Ingresar a menu \n2.salir \n"))
-    while opc1 != 0:
-        opc1 = opc1+1
-        opc=int(input("***BIENVENIDO A MENU VUELOSDUOC***\n----Porfavor escoja una opcion----\n1.Ver asientos Disponibles\n2.Comprar asientos \n3.Anular vuelo \n4.Modificar datos de usuario\n5.salir \n"))  
-        if opc ==1:
-            print("\n")
-            print("---------------------------------")
-            print("ingreso a ver asientos disponibles ")
-            print("---------------------------------")
-            print(puestosdeAvion)
-        elif opc ==2:
-            print("\n")
-            print("---------------------------------")
-            print("ingreso a a comprar asientos")
-            print("---------------------------------") 
-            NumeroAsiento=int(input("porfavor seleciones un asiento :"))
-            print(f"ha escogido {NumeroAsiento} ")
-        
+def MostrarVuelos(asientoVuelos):
+    print("\n")
+    print("---------------------------------")
+    print("ingreso a ver asientos disponibles ")
+    print("---------------------------------")
+    print(asientoVuelos)
+    #return"hola esto es un return y me devuelve un mensaje"
+    #esto funciona como otra parte de codigo añadida 
+    #print("esto no deberia salir")
+#------------------------------------------------------------------------------------
+def comprapasajes():
+    print("---------------------------------")
+    print("ingreso a a comprar asientos")
+    print("---------------------------------")
+    NumeroAsiento=int(input("porfavor seleciones un asiento :"))
+    print("ha escojido el asiento :",NumeroAsiento)
 
-        elif opc ==3:
-            print("\n")
-            print("---------------------------------")
-            print("ingreso a anular vuelo")
-            print("---------------------------------")
-        elif opc ==4:
-            print("\n")
-            print("---------------------------------")
-            print("ingreso Modificar datos de pasajero")
-            print("---------------------------------")
-        elif opc ==5:
-            print("\n")
-            print("---------------------------------")
-            print("Gracias por usar vuelosduoc")
-            print("---------------------------------")
-            break
-   
-        return opc1
-    
-Menu1=Menu()
+#def Anularpasaje():
+#def Modificardatos():
 
-    #------------------------------------------------------------------------------------
-
+def anularVuelo():
+    print("\n")
+    print("---------------------------------")
+    print("ingreso a anular vuelo")
+    print("---------------------------------")
+    print("ingrese sus datos")
--- vuelos.py/proyectosvuelos.py
+++ vuelos.py/proyectosvuelos.py
@@ -1,4 +1,39 @@
+import funciones as funcionVuelos
+
+print("Bienvenido a vuelos duoc")
+Ingresodatos=funcionVuelos.DatosPasajero()
+#primero se crea matriz para indicializar los puestos de avion 
+puestosdeAvion=funcionVuelos.crearmatrix(7,6)
 
 
+opc1=int(input("1.Ingresar a menu \n2.salir \n"))
+while opc1 != 0:
+    opc1 = opc1+1
+    opc=int(input("***BIENVENIDO A MENU VUELOSDUOC***\n----Porfavor escoja una opcion----\n1.Ver asientos Disponibles\n2.Comprar asientos \n3.Anular vuelo \n4.Modificar datos de usuario\n5.salir \n"))  
+    if opc ==1:
+        #primero se debe llamar la caja fuerte luego llamamos la funcion creada y le entregamos la variable que contiene la funcion en este caso puestosdeAvion
+        respuesta=funcionVuelos.MostrarVuelos(puestosdeAvion)
+        print(respuesta)
+    elif opc ==2:
+        print("---------------------------------")
+        print("ingreso a a comprar asientos")
+        print("---------------------------------")
+        NumeroAsiento=int(input("porfavor seleciones un asiento :"))
+        print("********************************************")
+        print("ha escojido el asiento :",NumeroAsiento)
+        print("********************************************")
+    elif opc ==3:
+         print("---------------------------------")
+    elif opc ==4:
+        print("\n")
+        print("---------------------------------")
+        print("ingreso Modificar datos de pasajero")
+        print("---------------------------------")
+    elif opc ==5:
+        print("\n")
+        print("---------------------------------")
+        print("Gracias por usar vuelosduoc")
+        print("---------------------------------")
+        break
 
 
